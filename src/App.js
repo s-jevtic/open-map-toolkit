9b773672cec92a190e1fac80533d1f5ab757fbec
sjevtic <sjevtic@protonmail.com>
2023-03-17 18:32:26 +0100

    update layout and edit page metadata

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
 		console.log(map);
 	}
 	return (
-		<div className="App h-full">
-			<header className="App-header flex justify-center bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-50/80 py-3">
+		<div className="App h-full flex flex-col">
+			<header className="App-header flex justify-center bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-50/80 py-3 drop-shadow-2xl z-20 opacity-80">
 				<h1>OpenMapToolkit</h1>
 			</header>
-			<main className="App-main h-full">
+			<main className="App-main z-10 absolute top-0 bottom-0 left-0 right-0">
 				<Map mapReadyCallback={mapReadyCallback}/>
 			</main>
 		</div>
