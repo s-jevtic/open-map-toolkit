95d1744183a5607db729e2ec773b313fb06e14e3
sjevtic <sjevtic@protonmail.com>
2023-04-19 16:38:46 +0200

    improve tool UI (WIP)

--- src/ToolUi.js
+++ src/ToolUi.js
@@ -8,8 +8,13 @@ function ToolUi({ toolId, onToolDeselect }) {
   }
   return (
     <>
-      <div className="flex-none dark:bg-slate-900 text-slate-900 dark:text-slate-50/80 md:opacity-80 opacity-100 md:w-96 w-full h-32">
-        <Tool toolId={toolId} units={units} />
+      <div
+        id="tool-ui"
+        className="absolute right-0 border-[5px] lg:border-r-0 lg:rounded-l-md bg-slate-50 border-slate-900 dark:border-transparent dark:bg-slate-900 text-slate-900 dark:text-slate-50/80 lg:opacity-80 opacity-100 lg:w-96 w-full lg:h-96 max-h-32 p-2 lg:my-12 flex flex-col justify-between"
+      >
+        <div id="tool-container" className="text-center">
+          <Tool toolId={toolId} units={units} />
+        </div>
         <button onClick={onToolDeselect}>close</button>
         {/*TODO*/}
       </div>
